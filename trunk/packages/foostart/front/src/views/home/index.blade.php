4939f37a46641e8586c01274f152c8b4f5bb66f6
dt <thiennguyenduc1999@gmail.com>
2017-04-04 10:48:49 +0700

    update
    Phát

--- trunk/packages/foostart/front/src/views/home/index.blade.php
+++ trunk/packages/foostart/front/src/views/home/index.blade.php
@@ -4,6 +4,6 @@
      </head>
      <body>
          <h1>hello</h1>
-         <a href="{{route('news')}}">Tin Tuc</a>
+         <a href="{{route('tin-tuc')}}">Tin Tuc</a>
      </body>
  </html>
--- trunk/packages/foostart/front/src/views/news/new.blade.php
+++ trunk/packages/foostart/front/src/views/news/new.blade.php
@@ -3,6 +3,6 @@
          <title></title>
      </head>
      <body>
-         <h1>đây là trang news</h1>
+         <h1>đây là trang tin tức</h1>
      </body>
  </html>
