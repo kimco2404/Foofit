46e4f9106c9a428368bea8fb50f4aed6201677fc
tanphat <tanphat0196@gmail.com>
2017-04-28 08:35:39 +0700

    cm
    cm

--- trunk/packages/front/src/views/modules/341.php
+++ trunk/packages/front/src/views/modules/341.php
@@ -1,77 +1,63 @@
-
 <div class="type-341">
-    <div class="container">
+    <div class="container training">
         <div class="container brochure">
             <div class="row">
                 <!--BROCHURE-->
-                <div class="container">
-                    <!-- Next button -->
-                    <!-- Previous button -->
-                </div>
-                <div class="flipbook-viewport" style="background-image: url('');">
-                    <div class="flipbook-viewport">
-                        <div class="container">
-                            <div class="flipbook">
-                                <div style="background-image:url(front/images/iTDCcatalog_print1.jpg)"></div>
-                                <div style="background-image:url(front/images/iTDCcatalog_print2.jpg)"></div>
-                                <div style="background-image:url(front/images/iTDCcatalog_print3.jpg)"></div>
-                                <div style="background-image:url(front/images/iTDCcatalog_print4.jpg)"></div>
-                                <div style="background-image:url(front/images/iTDCcatalog_print5.jpg)"></div>
-                                <div style="background-image:url(front/images/iTDCcatalog_print6.jpg)"></div>
-                                <div style="background-image:url(front/images/iTDCcatalog_print7.jpg)"></div>
-                                <div style="background-image:url(front/images/iTDCcatalog_print8.jpg)"></div>
-                                <div style="background-image:url(front/images/iTDCcatalog_print9.jpg)"></div>
-                                <div style="background-image:url(front/images/iTDCcatalog_print10.jpg)"></div>
-                                <div style="background-image:url(front/images/iTDCcatalog_print11.jpg)"></div>
-                                <div style="background-image:url(front/images/iTDCcatalog_print12.jpg)"></div>
-                                <div style="background-image:url(front/images/iTDCcatalog_print13.jpg)"></div>
-                                <div style="background-image:url(front/images/iTDCcatalog_print14.jpg)"></div>
-                                <div style="background-image:url(front/images/iTDCcatalog_print15.jpg)"></div>
-                                <div style="background-image:url(front/images/iTDCcatalog_print16.jpg)"></div>
-                            </div>
+                <div class="magazine-viewport">
+                    <div class="container">
+                        <div class="magazine">
+                            <!-- Next button -->
+                            <div ignore="1" class="next-button"></div>
+                            <!-- Previous button -->
+                            <div ignore="1" class="previous-button"></div>
                         </div>
                     </div>
                 </div>
+                <div class="flipbook-viewport" style="background:url('');">
+                    <div class="container_mag">
+                        <div class="flipbook">
+                            <div style="background-image:url(front/images/iTDCcatalog_print1.jpg)"></div>
+                            <div style="background-image:url(front/images/iTDCcatalog_print2.jpg)"></div>
+                            <div style="background-image:url(front/images/iTDCcatalog_print3.jpg)"></div>
+                            <div style="background-image:url(front/images/iTDCcatalog_print4.jpg)"></div>
+                            <div style="background-image:url(front/images/iTDCcatalog_print5.jpg)"></div>
+                            <div style="background-image:url(front/images/iTDCcatalog_print6.jpg)"></div>
+                            <div style="background-image:url(front/images/iTDCcatalog_print7.jpg)"></div>
+                            <div style="background-image:url(front/images/iTDCcatalog_print8.jpg)"></div>
+                            <div style="background-image:url(front/images/iTDCcatalog_print9.jpg)"></div>
+                            <div style="background-image:url(front/images/iTDCcatalog_print10.jpg)"></div>
+                            <div style="background-image:url(front/images/iTDCcatalog_print11.jpg)"></div>
+                            <div style="background-image:url(front/images/iTDCcatalog_print12.jpg)"></div>
+                            <div style="background-image:url(front/images/iTDCcatalog_print13.jpg)"></div>
+                            <div style="background-image:url(front/images/iTDCcatalog_print14.jpg)"></div>
+                            <div style="background-image:url(front/images/iTDCcatalog_print15.jpg)"></div>
+                            <div style="background-image:url(front/images/iTDCcatalog_print16.jpg)"></div>
+                        </div>
+                    </div>
+                </div>                
             </div>
             <!--/END BROCHURE-->
         </div>
     </div>
 </div>
-<script type="text/javascript">
+<script>
+    var oTurn = $(".flipbook").turn({
+        width: 960,
+        height: 338,
+        elevation: 50,
+        gradients: true,
+        autoCenter: true,
+        next: true
+    });
 
-    function loadApp() {
+    $("#prev").click(function (e) {
+        e.preventDefault();
+        oTurn.turn("previous");
+    });
 
-        // Create the flipbook
-
-        $('.flipbook').turn({
-            // Width
-
-            width: 960,
-            height: 338,
-            elevation: 50,
-            gradients: true,
-            autoCenter: true,
-            next: true
-
-        });
-
-        $("#prev").click(function (e) {
-            e.preventDefault();
-            oTurn.turn("previous");
-        });
-
-        $("#next").click(function (e) {
-            e.preventDefault();
-            oTurn.turn("next");
-        });
-
-    }
-
-    // Load the HTML4 version if there's not CSS transform
-
-
+    $("#next").click(function (e) {
+        e.preventDefault();
+        oTurn.turn("next");
+    });
 </script>
-<style>
-
-</style>
 
